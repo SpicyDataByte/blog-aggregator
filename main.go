a524f857ffe90e910a507b7b44ed18d50921974a
SpicyDataByte <matt@hildebrandt.club>
2025-04-22 20:51:08 -0600

    reset to config

--- main.go
+++ main.go
@@ -1,41 +1,25 @@
 package main
 
 import (
+	"fmt"
 	"log"
-	"os"
 
 	"github.com/SpicyDataByte/blog-aggregator/internal/config"
-)
 
-type state struct {
-	cfg *config.Config
-}
+)
 
 func main() {
 	cfg, err := config.Read()
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
+	fmt.Printf("Read config: %+v\n", cfg)
 
-	programState := &state{
-		cfg: &cfg,
-	}
+	err = cfg.SetUser("lane")
 
-	cmds := commands{
-		registeredCommands: make(map[string]func(*state, command) error),
-	}
-	cmds.register("login", handlerLogin)
-
-	if len(os.Args) < 2 {
-		log.Fatal("Usage: cli <command> [args...]")
-		return
-	}
-
-	cmdName := os.Args[1]
-	cmdArgs := os.Args[2:]
-
-	err = cmds.run(programState, command{Name: cmdName, Args: cmdArgs})
+	cfg, err = config.Read()
 	if err != nil {
-		log.Fatal(err)
+		log.Fatalf("error reading config: %v", err)
 	}
+	fmt.Printf("Read config again: %+v\n", cfg)
 }
