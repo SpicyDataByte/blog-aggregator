bad936f1d32a207d493b9d0b6246de11c4737bf4
SpicyDataByte <matt@hildebrandt.club>
2025-04-23 12:26:10 -0600

    handler_user

--- main.go
+++ main.go
@@ -1,14 +1,16 @@
 package main
-
 import (
 	"log"
 	"os"
-
 	"github.com/SpicyDataByte/blog-aggregator/internal/config"
+	_ "github.com/lib/pq"
+	"database/sql"
+	"github.com/SpicyDataByte/blog-aggregator/internal/database"
 
 )
 
 type state struct {
+	db  *database.Queries
 	cfg *config.Config
 }
 
@@ -17,8 +19,15 @@ func main() {
 	if err != nil {
 		log.Fatalf("error reading config: %v", err)
 	}
+	db, err := sql.Open("postgres", cfg.DBURL)
+	if err != nil {
+		log.Fatalf("error connecting to db: %v", err)
+	}
+	defer db.Close()
+	dbQueries := database.New(db)
 
 	programState := &state{
+		db:  dbQueries,
 		cfg: &cfg,
 	}
 
@@ -26,6 +35,7 @@ func main() {
 		registeredCommands: make(map[string]func(*state, command) error),
 	}
 	cmds.register("login", handlerLogin)
+	cmds.register("register", handlerRegister)
 
 	if len(os.Args) < 2 {
 		log.Fatal("Usage: cli <command> [args...]")
